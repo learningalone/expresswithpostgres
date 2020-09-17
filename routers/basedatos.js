2fbaa4cbac831a9e5c97395f3589972bd1ec02e5
Alejandro Caicedo <alejandrocaicedo21216@gmail.com>
2020-09-17 13:42:10 -0500

    Commit API postgres

--- routers/basedatos.js
+++ routers/basedatos.js
@@ -2,12 +2,13 @@ const { Pool } = require('pg');
 const Router = require('express-promise-router');
 
 const pool = new Pool({
-  user: 'fredyb',
+  user: 'postgres',
   host: 'localhost',
   database: 'postgres',
-  password: 'univalle',
+  password: 'AlejoCx2',
   port: 5432,
-});
+}
+);
 
 const router = new Router();
 // export our router to be mounted by the parent application
@@ -19,10 +20,29 @@ router.get('/consultatotalpacientes', async (req, res) => {
   res.send(rows);
 });
 
-router.post('/insertarpacientes', async (req, res) => {
+router.put('/insertarpacientes', async (req, res) => {
   const { nombre, apellido, numid } = req.body;
   await pool.query(
     `INSERT INTO pacientes(nombre, apellido, numid) VALUES('${nombre}','${apellido}','${numid}')`
   );
-  res.send('INSERTADO');
+  res.send('PACIENTE INSERTADO !!!');
+});
+
+
+router.post('/actualizarpacientes', async (req, res) => {
+  const { id,nombre,apellido,numid } = req.body;
+
+  await pool.query(
+    `UPDATE pacientes SET nombre ='${nombre}', apellido ='${apellido}', numid ='${numid}' WHERE id ='${id}'`
+  );
+  res.send('PACIENTE ACTUALIZADO !!!');
+});
+
+router.delete('/eliminarpacientes', async (req, res) => {
+  const { id } = req.body;
+
+  await pool.query(
+    `DELETE FROM pacientes WHERE id ='${id}'`
+  );
+  res.send('PACIENTE ELIMINADO !!!');
 });
